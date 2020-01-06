0758a91303bac67672e22328d1098428658271a3
nbosio1001 <49621575+nbosio1001@users.noreply.github.com>
2020-01-05 17:23:30 -0800

    Add files via upload
    Adding expenses_dict to GitHub. For some reason, git push origin master wasn't working.

---
+++ expenses_dict.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 17:01:37 2019
+
+@author: Nick Bosio
+"""
+def expenses_dict():
+    expenses_dict = {
+        'Groceries': [
+            'VONS',
+            'RALPHS',
+            'Ralphs',
+            'PAVILIONS',
+            'FOOD4LESS',
+            'TRADER JOE\'S',
+            'GROCERY OUTLET',
+            'FOOD 4 LESS',
+            'SPROUTS',
+            'MARKET@WORK',
+            'CALIFORNIA FRESH'
+            ],
+        'Meetups': [
+                'BLUE BUTTERFLYEL'
+                ],
+        'Rent and Utilities': [
+             'Rent',
+             'Internet',
+             'Water',
+             'Electricity'
+             ],
+         'Transportation': [
+                 'METROLINK',
+                 'EXXONMOBIL',
+                 '\<GAS\>',
+                 'UNION 76',
+                 'DMV',
+                 'OIL',
+                 'CIRCLE K',
+                 'LA METRO',
+                 'VONS FUEL'
+                 ],
+         'Travel': [
+                 'TRAVEL INSURANCE',
+                 'ALASKA AIR'
+                 ],
+         'Housing Supplies': [
+                 'LOWE\'S',
+                 'RITE AID'
+                 ],
+         'Car Maintenance': [
+                 'SMOG'
+                 ],
+         'Clothes': [
+                 'NIKE'
+                 ],
+         'Going Out': [
+                 '85C',
+                 'RESTAURA',
+                 'COSTCO',
+                 'SUBWAY',
+                 'IN N OUT',
+                 'Starbucks',
+                 'PIZZA',
+                 'TACO BELL',
+                 'SLO BREW'
+                 ],
+         'Salary': [
+                 'AEROTEK INC'
+                 ]
+         }
+    return expenses_dict
